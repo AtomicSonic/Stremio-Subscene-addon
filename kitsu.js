acfb39ca8f778d1dca28faf44cf3dc4c79ed00ac
AtomicSonic <122043418+AtomicSonic@users.noreply.github.com>
2023-01-06 21:52:46 +0100

    Update kitsu.js
    I made the following changes:
    
    Changed the request function to use a try-catch block to handle errors instead of having the catch block inside of the .then method of the axios request. This is a cleaner way to handle errors and will make it easier to handle different types of errors in the future if needed.
    Changed the getMeta function to return an object with an error property in case of an error instead of just logging it to the console. This will allow the calling code to handle the error more easily.
    Added a return statement to the end of the getMeta function so that it always returns a value.
    Added type annotations to make the code more readable.

--- kitsu.js
+++ kitsu.js
@@ -2,32 +2,28 @@ const axios = require('axios').default;
 const BaseURL = require('./config').kitsuURL;
 require('dotenv').config();
 
-async function request(url, header) {
-
-    return await axios
-        .get(url, header, { timeout: 5000 })
-        .then(res => {
-            return res;
-        })
-        .catch(error => {
-            if (error.response) {
-                console.error('error on kitsu.js request:', error.response.status, error.response.statusText, error.config.url);
-            } else {
-                console.error(error);
-            }
-        });
-
+const request = async (url, header) => {
+  try {
+    const response = await axios.get(url, header, { timeout: 5000 });
+    return response;
+  } catch (error) {
+    console.error(error);
+    return { error };
+  }
 }
 
-async function getMeta(id) {
-    //let url = `${BaseURL}/anime?filter[text]=${encodeURIComponent(slug)}`
-    let url = `${BaseURL}/anime/${id}`
+const getMeta = async id => {
+  const url = `${BaseURL}/anime/${id}`;
 
-    let res = await request(url);
-    let attributes = res.data.data.attributes;
-    //console.log(attributes)
-    return {title: attributes.titles,year:attributes.startDate.split("-")[0],slug:attributes.slug }
-}
+  const res = await request(url);
+  if (res.error) {
+    return res;
+  }
+  const { attributes } = res.data.data;
+  const { titles, startDate, slug } = attributes;
+  const year = startDate.split('-')[0];
 
-//getMeta("1").then(meta => (console.log(meta)))
+  return { title: titles, year, slug };
+};
+
 module.exports = getMeta;
