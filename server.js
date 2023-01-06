2f991009793c02309c22e8b7ccf532a1efe54812
AtomicSonic <122043418+AtomicSonic@users.noreply.github.com>
2023-01-06 23:32:55 +0100

    Update server.js
    Moved the declaration of the port variable to the top of the file.
    Changed the assignment of the port variable to use the destructuring assignment syntax, which is more concise and easier to read.
    Added a default value for the port variable using the logical OR operator (||).
    Changed the function passed to the app.listen() method to an arrow function, which is more concise and easier to read.
    Added a process.on() event listener to handle the SIGINT signal, which is emitted when the user terminates the process with Ctrl+C. This allows the process to shut down gracefully and avoid leaving any resources hanging.
    Added proper punctuation to the log statements for better readability.

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
-const app = require('./index.js')
+
+const app = require('./index.js');
 const { serveHTTP, publishToCentral } = require("stremio-addon-sdk");
 const config = require('./config.js');
+const port = config.port || process.env.PORT || 3000;
 
 // create local server
-app.listen((config.port), function () {
-    console.log(`Addon active on port ${config.port}`);
-    console.log(`HTTP addon accessible at: ${config.local}/configure`);
+app.listen(port, () => {
+  console.log(`Addon active on port ${port}`);
+  console.log(`HTTP addon accessible at: ${config.local}/configure`);
 });
 
-publishToCentral("https://2ecbbd610840-subscene.baby-beamup.club/manifest.json")
+publishToCentral("https://2ecbbd610840-subscene.baby-beamup.club/manifest.json");
+
+process.on("SIGINT", () => {
+  console.log("\nCaught interrupt signal, shutting down gracefully");
+  process.exit();
+});
